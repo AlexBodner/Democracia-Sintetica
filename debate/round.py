f599b21a00a6a6ca6e36cb8cc4f327df8801d054
delfinachavez <dchavezblasi@udesa.edu.ar>
2025-05-30 16:56:09 -0300

    Buen debate

--- debate/round.py
+++ debate/round.py
@@ -16,10 +16,11 @@ class FirstRound(Round):
 class SecondRound(Round):
     def __init__(self, law):
         super().__init__(law)
-        self.prompt = f"En esta ronda cada agente recibe como contexto previo los argumentos de todos los agentes de la primera ronda " \
+        self.prompt = f"En esta ronda cada agente recibe como contexto previo los argumentos de todos los agentes de la primera ronda (inlcuso lo que dijo el mismo) " \
                       "y van a poder contraargumentar o reafirmar su postura. Deben aclarar a qué agente le están respondiendo. " \
                       "Los agentes pueden intentar convencer al otro o cambiar su postura. Es importante que los agentes no se repitan y que\
-                      cada uno aporte algo nuevo y sean fieles a su postura política."
+                      cada uno aporte algo nuevo y sean fieles a su postura política. Tiene que seguir el planteo que hizo en la ronda\
+                      anterior, pero puede cambiar de opinión si lo considera necesario, siempre aclarando por que cambia de opinion y argumentando su decision. " 
         self.round_nr = 1
                       
 class ThirdRound(Round):
